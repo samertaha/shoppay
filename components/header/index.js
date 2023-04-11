a995b073b13eb9c9b44cb287435d040731340999
Samer taha <alrazy1@gmail.com>
2023-04-11 21:17:56 +0300

    built ad header component

---
+++ components/header/index.js
@@ -0,0 +1,9 @@
+import Ad from "./Ad"
+import styles from './styles.module.scss'
+export default function Header() {
+  return (
+      <header className={styles.header}>
+          <Ad />
+    </header>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,24 @@ import { Provider } from "react-redux"
 import store from "../store"
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
+import Head from 'next/head'
+
 let persistor = persistStore(store)
 export default function MyApp({ Component, pageProps }) {
-  return(
+  return (
+    <>
+      <Head>
+        <title>Shoppay</title>
+        <meta name="description" content="Shoppay-online shopping service for all of your needs." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
     <Provider store={store}>
       <PersistGate Loading={null} persistor={persistor}>
         <Component {...pageProps} />
       </PersistGate>
-   </Provider>
+      </Provider>
+    </>
   )
 }
