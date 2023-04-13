d11a3d8b373d67882983a5419ce0670aa5a9916b
Samer taha <alrazy1@gmail.com>
2023-04-13 07:44:52 +0300

    search component and styling

--- components/header/Top.js
+++ components/header/Top.js
@@ -7,42 +7,46 @@ import { useState } from 'react';
 import UserMenu from './UserMenu';
 
 export default function Top() {
-    const [loggedIn, setLoggedIn] = useState(false)
+    const [loggedIn, setLoggedIn] = useState(true)
+    const [visible, setVisible] = useState(false)
+
     return (
         <div className="styles.top">
             <div className={styles.top__container}>
                 <div></div>
                 <ul className={styles.top__list}>
-                    <li>
+                    <li className={styles.li}>
                         <img src="https://www.seekpng.com/png/detail/866-8665424_morocco-employability-and-land-compact-maroc-flag-ball.png" alt="" />
                         <span>Morrocco / usd</span>
                     </li>
-                    <li>
+                    <li className={styles.li}>
                         <MdSecurity />
                         <span>Buyer Protection</span>
                     </li>
-                    <li>
+                    <li className={styles.li}>
                         <span>Customer Service</span>
                     </li>
-                    <li>
+                    <li className={styles.li}>
                         <span>Hlep</span>
                     </li>
-                    <li>
+                    <li className={styles.li}>
                         <BsSuitHeart />
                         <Link href="/profile/wishlist">
                             <span>Wishlist</span>
                         </Link>
                     </li>
-                    <li>
+                    <li className={styles.li}
+                        onMouseOver={() => setVisible(true)}
+                        onMouseLeave={() => setVisible(false)}>
                         {
-                            loggedIn ? <li>
+                            loggedIn ? <li className={styles.li}>
                                 <div className={styles.flex}>
                                     <img src="https://cdn.pixabay.com/photo/2020/07/14/13/07/icon-5404125_960_720.png" alt="" />
                                     <span>Samer</span>
                                     <RiArrowDropDownFill />
                                 </div>
                             </li> :
-                                <li>
+                                <li className={styles.li}>
                                     <div className={styles.flex}>
                                         <RiAccountPinCircleLine />
                                         <span>Account</span>
@@ -50,7 +54,7 @@ export default function Top() {
                                     </div>
                                 </li>
                         }
-                        <UserMenu loggedIn={loggedIn} />
+                        {visible && <UserMenu loggedIn={loggedIn} />}
                     </li>
                 </ul>
             </div>
--- components/header/index.js
+++ components/header/index.js
@@ -1,11 +1,13 @@
+import Main from "./Main"
 import Ad from "./Ad"
 import Top from "./Top"
 import styles from './styles.module.scss'
 export default function Header() {
   return (
-      <header className={styles.header}>
+    <header className={styles.header}>
       <Ad />
       <Top />
+      <Main />
     </header>
   )
 }
