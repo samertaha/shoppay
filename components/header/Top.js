e996a512da15f29c28288c4679637e7d44e10f77
Samer taha <alrazy1@gmail.com>
2023-04-14 07:52:11 +0300

    added axios and ipregistry user location detiction

--- components/header/Top.js
+++ components/header/Top.js
@@ -1,23 +1,22 @@
-import styles from './styles.module.scss';
-import Link from 'next/link';
-import { MdSecurity } from 'react-icons/md';
-import { BsSuitHeart } from 'react-icons/bs';
+import styles from "./styles.module.scss";
+import { MdSecurity } from "react-icons/md";
+import { BsSuitHeart } from "react-icons/bs";
 import { RiAccountPinCircleLine, RiArrowDropDownFill } from "react-icons/ri";
-import { useState } from 'react';
-import UserMenu from './UserMenu';
-
-export default function Top() {
-    const [loggedIn, setLoggedIn] = useState(true);
+import Link from "next/link";
+import { useState } from "react";
+import UserMenu from "./UserMenu";
+import { useSession } from "next-auth/react";
+export default function Top({ country }) {
+    const { data: session } = useSession();
     const [visible, setVisible] = useState(false);
-
     return (
-        <div className="styles.top">
+        <div className={styles.top}>
             <div className={styles.top__container}>
                 <div></div>
                 <ul className={styles.top__list}>
                     <li className={styles.li}>
-                        <img src="https://www.seekpng.com/png/detail/866-8665424_morocco-employability-and-land-compact-maroc-flag-ball.png" alt="" />
-                        <span>Morrocco / usd</span>
+                        <img src={country?.flag} alt="" />
+                        <span>{country?.name} / USD</span>
                     </li>
                     <li className={styles.li}>
                         <MdSecurity />
@@ -27,36 +26,37 @@ export default function Top() {
                         <span>Customer Service</span>
                     </li>
                     <li className={styles.li}>
-                        <span>Hlep</span>
+                        <span>Help</span>
                     </li>
                     <li className={styles.li}>
                         <BsSuitHeart />
-                        <Link href="/profile/wishlist">
-                            <span>Wishlist</span>
+                        <Link href="/profile/whishlist">
+                            <span>Whishlist</span>
                         </Link>
                     </li>
-                    <li className={styles.li}
+                    <li
+                        className={styles.li}
                         onMouseOver={() => setVisible(true)}
-                        onMouseLeave={() => setVisible(false)}>
-                        <ul>
-                            {
-                                loggedIn ? <li className={styles.li}>
-                                    <div className={styles.flex}>
-                                        <img src="https://cdn.pixabay.com/photo/2020/07/14/13/07/icon-5404125_960_720.png" alt="" />
-                                        <span>Samer</span>
-                                        <RiArrowDropDownFill />
-                                    </div>
-                                </li> :
-                                    <li className={styles.li}>
-                                        <div className={styles.flex}>
-                                            <RiAccountPinCircleLine />
-                                            <span>Account</span>
-                                            <RiArrowDropDownFill />
-                                        </div>
-                                    </li>
-                            }
-                        </ul>
-                        {visible && <UserMenu loggedIn={loggedIn} />}
+                        onMouseLeave={() => setVisible(false)}
+                    >
+                        {session ? (
+                            <li className={styles.li}>
+                                <div className={styles.flex}>
+                                    <img src={session?.user?.image} alt="" />
+                                    <span>{session?.user?.name}</span>
+                                    <RiArrowDropDownFill />
+                                </div>
+                            </li>
+                        ) : (
+                            <li className={styles.li}>
+                                <div className={styles.flex}>
+                                    <RiAccountPinCircleLine />
+                                    <span>Account</span>
+                                    <RiArrowDropDownFill />
+                                </div>
+                            </li>
+                        )}
+                        {visible && <UserMenu session={session} />}
                     </li>
                 </ul>
             </div>
